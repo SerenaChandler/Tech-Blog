4389d30bc48a37d797bf682b2e2c6f1fa20f938e
Serena Chandler <serenachandler2019@gmail.com>
2021-05-02 17:58:00 -0700

    added model index

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const sequelize = require('../config/connection');
+
+router.get("/", (req, res) =>  {
+    res.render("homepage")
+})
